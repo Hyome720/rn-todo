b45d717462f4cac374554f83c6b680dffee8fbc0
Hyormone <ddf998@gmail.com>
2023-12-07 13:56:01 +0900

    :sparkles:아이디, 비밀번호 입력 칸 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './screens/SingInScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
-      <Text style={{ fontSize: 30 }}>Todo App</Text>
+      <SignInScreen />
     </View>
   );
 };
---
+++ src/screens/SingInScreen.js
@@ -0,0 +1,36 @@
+import { Image, StyleSheet, View } from 'react-native';
+import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
+
+const SignInScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/main.png')} style={styles.image} />
+
+      <Input
+        title={'이메일'}
+        placeholder={'your@email.com'}
+        keyboardType={KeyboardTypes.EMAIL}
+        returnKeyType={ReturnKeyTypes.NEXT}
+      />
+      <Input
+        title={'비밀번호'}
+        returnKeyType={ReturnKeyTypes.DONE}
+        secureTextEntry
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default SignInScreen;
